ae16d8d7891e1a31ca894dbd5937dba49949fb6e
andreas.grimme <andreas.grimme@6ee6a016-79e0-11de-888d-2103c0e6b660>
2009-09-06 17:00:23 +0000

    setup activated

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.0.58")]
-[assembly: AssemblyFileVersion("0.1.0.58")]
+[assembly: AssemblyVersion("0.1.0.60")]
+[assembly: AssemblyFileVersion("0.1.0.60")]
 
 

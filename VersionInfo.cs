e43e1060d33f2fc53539c779bd280f87acda4767
andreas.grimme <andreas.grimme@6ee6a016-79e0-11de-888d-2103c0e6b660>
2009-09-06 16:19:16 +0000

    Auto commit from MSBuild.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.0.45")]
-[assembly: AssemblyFileVersion("0.1.0.45")]
+[assembly: AssemblyVersion("0.1.0.47")]
+[assembly: AssemblyFileVersion("0.1.0.47")]
 
 

f2c2c88ff04b2c8bb3f3a1d50ac456195e6ecb2f
andreas.grimme <andreas.grimme@6ee6a016-79e0-11de-888d-2103c0e6b660>
2009-09-06 16:58:51 +0000

    Automatic build of hagen

---
+++ tags/0.1.0.60/hagen.wf/ClipboardUrl.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NUnit.Framework;
+using System.IO;
+using Sidi.IO;
+using System.Text.RegularExpressions;
+
+namespace hagen.wf
+{
+    public class ClipboardUrl
+    {
+        const string FileGroupDescriptorWFormat = "FileGroupDescriptorW";
+
+        public static bool TryParse(IDataObject data, out ClipboardUrl clipboardUrl)
+        {
+            try
+            {
+                var c = new ClipboardUrl();
+                var d = data.GetData(FileGroupDescriptorWFormat);
+                c.Title = Path.GetFileNameWithoutExtension(ReadFileDescriptorW((MemoryStream)d));
+                c.Url = ReadUrl((Stream)data.GetData("FileContents"));
+                clipboardUrl = c;
+                return true;
+            }
+            catch (Exception)
+            {
+                clipboardUrl = null;
+                return false;
+            }
+        }
+
+        public string Title { set; get; }
+        public string Url { set; get; }
+
+        static string ReadFileDescriptorW(Stream s)
+        {
+            s.Seek(76, SeekOrigin.Current);
+            var b = new BinaryReader(s);
+            byte[] fn = new byte[260 * 2];
+            s.Read(fn, 0, fn.Length);
+            string r = ASCIIEncoding.Unicode.GetString(fn);
+            return r.Substring(0, r.IndexOf((char)0));
+        }
+
+        static string ReadUrl(Stream s)
+        {
+            var r = new StreamReader(s);
+            for (; ; )
+            {
+                var line = r.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                var m = Regex.Match(line, @"URL=(?<url>.*)");
+                if (m.Success)
+                {
+                    return m.Groups["url"].Value;
+                }
+            }
+            throw new Exception();
+        }
+
+        /// <summary>
+        /// Test method dumps clipboard data formats to files
+        /// </summary>
+        /// <param name="data"></param>
+        public static void Dump(IDataObject data)
+        {
+            var f = data.GetFormats();
+            foreach (var i in f)
+            {
+                try
+                {
+                    var m = data.GetData(i) as MemoryStream;
+                    if (m != null)
+                    {
+                        File.WriteAllBytes(
+                            FileUtil.BinFile(FileUtil.CatDir(@"test", i)),
+                            m.ToArray());
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+
+        [TestFixture]
+        public class Test
+        {
+            [Test]
+            public void ReadFileDescriptor()
+            {
+                string fn = ClipboardUrl.ReadFileDescriptorW(File.OpenRead(FileUtil.BinFile(@"test\FileGroupDescriptorW")));
+                Assert.AreEqual("myCSharp.de - DIE C#- und .NET Community - GUI Windows-Forms Email aus Clipboard auslesen.URL", fn);
+            }
+
+            [Test]
+            public void ReadUrl()
+            {
+                string u = ClipboardUrl.ReadUrl(File.OpenRead(FileUtil.BinFile(@"test\FileContents")));
+                Assert.AreEqual("http://www.mycsharp.de/wbb2/thread.php?threadid=73296", u);
+            }
+        }
+    }
+}

960dfe021f25bfcfb7e6aca9a40e8c728fd0f5da
andreas.grimme <andreas.grimme@6ee6a016-79e0-11de-888d-2103c0e6b660>
2009-09-06 16:19:26 +0000

    Automatic build of hagen

---
+++ tags/0.1.0.49/activityReport/GraphEx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZedGraph;
+using System.Windows.Forms;
+using hagen;
+using Sidi.Persistence;
+using Sidi.Util;
+
+namespace activityReport
+{
+    public static class GraphEx
+    {
+        public static GraphPane CreateTimeGraph()
+        {
+            GraphPane pane = new GraphPane();
+            pane.IsFontsScaled = false;
+            pane.XAxis.Type = AxisType.Date;
+            pane.XAxis.MajorGrid.IsVisible = true;
+            pane.XAxis.Title.Text = "Time";
+            pane.YAxis.MajorGrid.IsVisible = true;
+            // pane.XAxis.Scale.MajorUnit = DateUnit.Day;
+            // pane.XAxis.Scale.MajorStep = 28;
+            return pane;
+        }
+
+        public static ZedGraphControl AsControl(this PaneBase pane)
+        {
+            ZedGraphControl c = new ZedGraphControl();
+            if (pane is MasterPane)
+            {
+                c.MasterPane = (MasterPane)pane;
+                c.IsSynchronizeXAxes = true;
+            }
+            else
+            {
+                c.GraphPane = (GraphPane)pane;
+            }
+            c.AxisChange();
+            return c;
+        }
+
+        const string dateFmt = "yyyy-MM-dd HH:mm:ss";
+
+        public static IEnumerable<Input> Range(this Collection<Input> inputs, DateTime begin, DateTime end)
+        {
+            string q = "begin >= {0} and end <= {1}".F(
+                begin.ToString(dateFmt).Quote(),
+                end.ToString(dateFmt).Quote());
+            return inputs.Select(q);
+        }
+
+        public static TimeSpan Active(this IEnumerable<Input> data)
+        {
+            return data.Aggregate(TimeSpan.Zero, (a, x) =>
+                        {
+                            return a.Add(x.End - x.Begin);
+                        });
+        }
+
+
+    }
+}
---
+++ tags/0.1.0.49/hagen.core/Hagen.cs
@@ -0,0 +1,70 @@
+﻿using Sidi.IO;
+using System;
+using Sidi.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hagen
+{
+    public class Hagen
+    {
+        public string DatabasePath
+        {
+        get
+        {
+            return FileUtil.CatDir(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "hagen",
+                "hagen.sqlite");
+        }
+        }
+
+        public Collection<Action> Actions
+        {
+            get
+            {
+                return new Collection<Action>(DatabasePath);
+            }
+        }
+
+        public Collection<Input> Inputs
+        {
+            get
+            {
+                return new Collection<Input>(DatabasePath);
+            }
+        }
+
+        public Collection<ProgramUse> ProgramUses
+        {
+            get
+            {
+                return new Collection<ProgramUse>(DatabasePath);
+            }
+        }
+
+        public Collection<Log> Logs
+        {
+            get
+            {
+                return new Collection<Log>(DatabasePath);
+            }
+        }
+
+        public static Hagen Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Hagen();
+                }
+                return instance;
+            }
+        }
+
+        static Hagen instance;
+    }
+}

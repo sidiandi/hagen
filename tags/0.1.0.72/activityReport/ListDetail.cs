70e03e35a3081ff4df514b7f4eff9e7a6909f8e6
andreas.grimme <andreas.grimme@6ee6a016-79e0-11de-888d-2103c0e6b660>
2010-01-23 06:43:16 +0000

    Automatic build of hagen

---
+++ tags/0.1.0.72/activityReport/ListDetail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace activityReport
+{
+    public delegate Control CreateDetailView();
+    
+    public partial class ListDetail : Form
+    {
+        public ListDetail()
+        {
+            InitializeComponent();
+        }
+
+        public void AddItem(string text, CreateDetailView createDetailView)
+        {
+            ListViewItem i = new ListViewItem(text);
+            i.Tag = createDetailView;
+            List.Items.Add(i);
+        }
+
+        private void List_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CreateDetailView c = List.FocusedItem.Tag as CreateDetailView;
+            Splitter.Panel2.Controls.Clear();
+            if (c != null)
+            {
+                Control d = c();
+                d.Dock = DockStyle.Fill;
+                d.Visible = true;
+                Splitter.Panel2.Controls.Add(d);
+            }
+        }
+    }
+}
---
+++ tags/0.1.0.72/hagen.wf/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace hagen.wf
+{
+    static class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            KillAlreadyRunning();
+
+            using (var activityLogger = Debugger.IsAttached ? null : new ActivityLogger())
+            {
+                log.Info("Startup");
+                var main = new Main();
+                Application.Run();
+                Application.Exit();
+                log.Info("Shutdown");
+            }
+        }
+
+        /// <summary>
+        /// Kills all other already running processes with the same file name
+        /// </summary>
+        static void KillAlreadyRunning()
+        {
+            Process thisProcess = Process.GetCurrentProcess();
+            string thisProcessFileName = Path.GetFileName(thisProcess.MainModule.FileName);
+            foreach (var p in Process.GetProcesses().Where(x =>
+                {
+                    try
+                    {
+                        if (x.Id == thisProcess.Id)
+                        {
+                            return false;
+                        }
+
+                        string fn = Path.GetFileName(x.MainModule.FileName);
+                        return fn == thisProcessFileName;
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                }))
+            {
+                p.Kill();
+            }
+        }
+    }
+}

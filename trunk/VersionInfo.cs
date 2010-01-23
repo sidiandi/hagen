7054a4af85926c959e07df2e011faa492b9d65b9
andreas.grimme <andreas.grimme@6ee6a016-79e0-11de-888d-2103c0e6b660>
2010-01-23 18:56:07 +0000

    exit prompt

--- trunk/VersionInfo.cs
+++ trunk/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.0.93")]
-[assembly: AssemblyFileVersion("0.1.0.93")]
+[assembly: AssemblyVersion("0.1.0.94")]
+[assembly: AssemblyFileVersion("0.1.0.94")]
 
 

d75d1c7a783575fcfff60d2bc6e862e672ab375e
andreas.grimme <andreas.grimme@6ee6a016-79e0-11de-888d-2103c0e6b660>
2010-01-28 04:44:47 +0000

    text report

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.0.102")]
-[assembly: AssemblyFileVersion("0.1.0.102")]
+[assembly: AssemblyVersion("0.1.0.103")]
+[assembly: AssemblyFileVersion("0.1.0.103")]
 
 

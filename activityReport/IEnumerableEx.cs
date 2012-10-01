62327242b906117d86cd82ba002dc2349e4087f9
andreas.grimme <andreas.grimme@6ee6a016-79e0-11de-888d-2103c0e6b660>
2012-10-01 19:50:33 +0000

    worktime calculation

---
+++ activityReport/IEnumerableEx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace activityReport
+{
+    public static class IEnumerableEx
+    {
+        public static IEnumerable<KeyValuePair<T, double>> SlidingSum<T>(this IEnumerable<T> e, Func<T, double> selector, int window)
+        {
+            double sum = 0;
+            var sub = new double[window].Concat(e.Select(x => selector(x)));
+            var eSub = sub.GetEnumerator();
+            return e.Select(i =>
+                {
+                    sum += selector(i);
+                    eSub.MoveNext();
+                    sum -= eSub.Current;
+                    return new KeyValuePair<T, double>(i, sum);
+                });
+        }
+    }
+}

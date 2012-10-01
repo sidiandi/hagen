237a19723771b0c86b10070b14857fff25046a3a
andreas.grimme <andreas.grimme@6ee6a016-79e0-11de-888d-2103c0e6b660>
2012-10-01 19:50:33 +0000

    worktime calculation

---
+++ trunk/activityReport/Worktime.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using hagen;
+
+namespace activityReport
+{
+    ///Calculate work time according to German regulations
+    public class Worktime
+    {
+        public static TimeSpan Pause = TimeSpan.FromHours(0.75);
+        public static TimeSpan MaxWorkTimeWithoutPause = TimeSpan.FromHours(6);
+        public static TimeSpan MaxWorkTimePerDay = TimeSpan.FromHours(10);
+        public static TimeSpan RegularDailyWorkTime = TimeSpan.FromHours(8);
+
+        public static bool IsWorkDay(DateTime date)
+        {
+            switch (date.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Saturday:
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        public static TimeSpan GetOfficialWorkTime(TimeInterval time)
+        {
+            var d = time.Duration;
+            if (d <= MaxWorkTimeWithoutPause)
+            {
+                return time.Duration;
+            }
+
+            if (d <= MaxWorkTimeWithoutPause + Pause)
+            {
+                return MaxWorkTimeWithoutPause;
+            }
+
+            d = d - Pause;
+
+            if (d < MaxWorkTimePerDay)
+            {
+                return d;
+            }
+
+            return MaxWorkTimePerDay;
+        }
+    }
+}

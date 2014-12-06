d040ecd53333ecfd0d2ba5664c1f7f8c45595692
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@6ee6a016-79e0-11de-888d-2103c0e6b660>
2014-12-06 06:31:08 +0000

    Automatic build of hagen

--- trunk/VersionInfo.cs
+++ trunk/VersionInfo.cs
@@ -11,7 +11,7 @@
 [assembly: System.Reflection.AssemblyCopyright("Copyright (c) Andreas Grimme 2008-2014")]
 [assembly: System.Reflection.AssemblyCompany("sidi")]
 [assembly: System.Reflection.AssemblyProduct("sidi-util")]
-[assembly: System.Reflection.AssemblyVersion("0.5.0.2")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.2")]
+[assembly: System.Reflection.AssemblyVersion("0.5.0.3")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.3")]
 
 

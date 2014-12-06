5e155cdf5cfc22f6b3e967b5280bdabf50d37eab
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@6ee6a016-79e0-11de-888d-2103c0e6b660>
2014-12-06 07:22:30 +0000

    Automatic build of hagen

---
+++ tags/0.5.0.8/hagen.ext/PathListExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sidi.IO;
+
+namespace hagen
+{
+    public static class PathListExtension
+    {
+        public static string JoinTruncated<T>(this IEnumerable<T> list, string sep, int maxLength)
+        {
+            var sb = new StringBuilder();
+            var e = list.GetEnumerator();
+            if (e.MoveNext())
+            {
+                for (; ; )
+                {
+                    sb.Append(e.Current.ToString());
+                    if (e.MoveNext())
+                    {
+                        sb.Append(sep);
+                        if (sb.Length > maxLength)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            int i = 0;
+            for (; ; ++i)
+            {
+                if (!e.MoveNext())
+                {
+                    break;
+                }
+            }
+            if (i > 0)
+            {
+                sb.AppendFormat("and {0} more", i);
+            }
+            return sb.ToString();
+        }
+    }
+}

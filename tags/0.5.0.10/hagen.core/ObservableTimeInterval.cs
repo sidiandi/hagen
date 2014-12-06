c365c21e0acc3f933f74a5ce6167c58411ddc689
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@6ee6a016-79e0-11de-888d-2103c0e6b660>
2014-12-06 07:26:22 +0000

    Automatic build of hagen

---
+++ tags/0.5.0.10/hagen.core/ObservableTimeInterval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hagen
+{
+    public class ObservableTimeInterval
+    {
+        public IObservable<DateTime> Begin { get; set; }
+        public IObservable<DateTime> End { get; set; }
+
+        public static IObservable<ObservableTimeInterval> GetSeconds()
+        {
+            return Get(TimeSpan.FromSeconds(1));
+        }
+
+        public static IObservable<ObservableTimeInterval> Get(TimeSpan intervalTime)
+        {
+            return Observable.Interval(intervalTime)
+                .Select(_ => DateTime.Now)
+                .Window(2, 1)
+                .Select(_ => new ObservableTimeInterval { Begin = _.Take(1), End = _.Skip(1).Take(1) });
+        }
+    }
+}

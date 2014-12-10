a28ecb72b7e14e6f7a7b1f3a9a1bafc35f4f840b
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@6ee6a016-79e0-11de-888d-2103c0e6b660>
2014-12-10 04:02:51 +0000

    Automatic build of hagen

---
+++ tags/0.6.0.21/hagen.core/AggregateUtil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sidi.Extensions;
+using System.Windows;
+using System.Reactive.Linq;
+
+namespace hagen
+{
+    public static class AggregateUtil
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static double Distance(System.Drawing.Point p0, System.Drawing.Point p1)
+        {
+            var length = (new Vector(p0.X, p0.Y) - new Vector(p1.X, p1.Y)).Length;
+            return length;
+        }
+
+        public static string Details(this object x)
+        {
+            return StringExtensions.ToString(o => x.DumpProperties(o));
+        }
+
+        public static IObservable<double> ToDistance(this IObservable<MouseEventArgs> e)
+        {
+            return e.Buffer(2, 1)
+                .Select(b => 
+                    {
+                        var d = Distance(b[0].Location, b[1].Location);
+                        if (d >100)
+                        {
+                            log.Info(b[0].Details());
+                            log.Info(b[1].Details());
+                            log.Info(d);
+                        }
+                        return d;
+                    });
+        }
+    }
+}
---
+++ tags/0.6.0.21/hagen.plugin.file/InsertFilesAsText.cs
@@ -0,0 +1,44 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of hagen.
+// 
+// hagen is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// hagen is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with hagen. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Sidi.Util;
+using NUnit.Framework;
+using Sidi.CommandLine;
+using Sidi.Extensions;
+
+namespace hagen
+{
+    [Usage("Actions for files")]
+    public class FileActions
+    {
+        [Usage("inserts files in clipboard as text")]
+        public void InsertFilesAsText()
+        {
+            if (Clipboard.ContainsFileDropList())
+            {
+                var text = Clipboard.GetFileDropList().Cast<string>().Join();
+                Clipboard.SetText(text);
+                SendKeys.Send("+{INS}");
+            }
+        }
+    }
+}

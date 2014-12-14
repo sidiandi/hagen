8c70d5a4f797a8b11e69f1e9ceeece36955ec82c
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@6ee6a016-79e0-11de-888d-2103c0e6b660>
2014-12-14 06:33:37 +0000

    Automatic build of hagen

---
+++ tags/0.6.0.51/activityReport/GraphEx.cs
@@ -0,0 +1,61 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of hagen.
+// 
+// hagen is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// hagen is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with hagen. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZedGraph;
+using System.Windows.Forms;
+using hagen;
+using Sidi.Persistence;
+using Sidi.Util;
+
+namespace activityReport
+{
+    public static class GraphEx
+    {
+        public static GraphPane CreateTimeGraph()
+        {
+            GraphPane pane = new GraphPane();
+            pane.IsFontsScaled = false;
+            pane.XAxis.Type = AxisType.Date;
+            pane.XAxis.MajorGrid.IsVisible = true;
+            pane.XAxis.Title.Text = "Time";
+            pane.YAxis.MajorGrid.IsVisible = true;
+            // pane.XAxis.Scale.MajorUnit = DateUnit.Day;
+            // pane.XAxis.Scale.MajorStep = 28;
+            return pane;
+        }
+
+        public static ZedGraphControl AsControl(this PaneBase pane)
+        {
+            ZedGraphControl c = new ZedGraphControl();
+            if (pane is MasterPane)
+            {
+                c.MasterPane = (MasterPane)pane;
+                c.IsSynchronizeXAxes = true;
+            }
+            else
+            {
+                c.GraphPane = (GraphPane)pane;
+            }
+            c.AxisChange();
+            return c;
+        }
+    }
+}
---
+++ tags/0.6.0.51/hagen.core/TimeIntervalExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sidi.Util;
+
+namespace hagen
+{
+    public static class TimeIntervalExtensions
+    {
+        public static TimeInterval LastDays(int days)
+        {
+            var today = DateTime.Now.Date;
+            return new TimeInterval(today.AddDays(-days), today.AddDays(1));
+        }
+
+        public static bool Contains(this TimeInterval timeInterval, DateTime t)
+        {
+            return timeInterval.Begin <= t && t < timeInterval.End;
+        }
+    }
+}

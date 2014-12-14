c302cdfc5458a1fc4782fc9d597605b86e2a517a
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@6ee6a016-79e0-11de-888d-2103c0e6b660>
2014-12-14 06:32:40 +0000

    Automatic build of hagen

---
+++ tags/0.6.0.49/trunk/hagen.core/ActionSource/Filter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hagen.ActionSource
+{
+    public class Filter : IActionSource
+    {
+        public Filter(IActionSource source, Func<IEnumerable<IAction>, IEnumerable<IAction>> filter)
+        {
+            this.source = source;
+            this.filter = filter;
+        }
+
+        IActionSource source;
+        Func<IEnumerable<IAction>, IEnumerable<IAction>> filter;
+    
+        public IEnumerable<IAction>  GetActions(string query)
+        {
+ 	        return filter(source.GetActions(query));
+        }
+    }
+}
---
+++ tags/0.6.0.49/trunk/hagen/SearchBox.cs
@@ -0,0 +1,317 @@
+// Copyright (c) 2012, Andreas Grimme (http://andreas-grimme.gmxhome.de/)
+// 
+// This file is part of hagen.
+// 
+// hagen is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// hagen is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with hagen. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Sidi.Persistence;
+using Sidi.Collections;
+using Sidi.Util;
+using System.IO;
+using Sidi.IO;
+using hagen.ActionSource;
+using BrightIdeasSoftware;
+using Sidi.Extensions;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace hagen
+{
+    public partial class SearchBox : UserControl
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        ObjectListView itemView;
+
+        IActionSource2 m_actionSource;
+        IActionSource2 ActionSource
+        {
+            set
+            {
+                m_actionSource = value;
+            }
+
+            get
+            {
+                return m_actionSource;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+          if (!IsDisposed)
+          {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                    components = null;
+                }
+
+                Action.IconCache.EntryUpdated -= new EventHandler<LruCacheBackground<Action, Icon>.EntryUpdatedEventArgs>(IconCache_EntryUpdated);
+            }
+            // Free your own state (unmanaged objects).
+            // Set large fields to null.
+          }
+          base.Dispose(disposing);
+        }
+
+        void SelectItem(int index)
+        {
+            itemView.SelectedIndex = index;
+        }
+
+        public SearchBox(IActionSource2 actionSource)
+        {
+            int size = 40;
+
+            itemView = new ObjectListView()
+            {
+                HeaderStyle = ColumnHeaderStyle.None,
+                Dock = DockStyle.Fill,
+                TabStop = false,
+                RowHeight = size,
+                GridLines = false,
+                HideSelection = false,
+                UseCustomSelectionColors = true,
+                HighlightBackgroundColor = SystemColors.Highlight,
+                UnfocusedHighlightBackgroundColor = SystemColors.Highlight,
+                HighlightForegroundColor = SystemColors.HighlightText,
+                UnfocusedHighlightForegroundColor = SystemColors.HighlightText,
+                OwnerDraw = true,
+                ShowGroups = false,
+                ShowItemToolTips = true,
+                Sorting = SortOrder.None,
+                UseAlternatingBackColors = true,
+                AlternateRowBackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0),
+                UseHotItem = true,
+                FullRowSelect = true,
+            };
+
+            itemView.Columns.Add(new OLVColumn()
+                {
+                    Name = "Icon",
+                    AspectGetter = x => ((IAction)x).Name,
+                    AspectToStringConverter = x => String.Empty,
+                    ImageGetter = x =>
+                    {
+                        try
+                        {
+                            var icon = ((IAction)x).Icon;
+                            return icon != null ? icon.ToBitmap() : null;
+                        }
+                        catch
+                        {
+                            return null;
+                        }
+                    },
+                    Width = size,
+                });
+
+            itemView.Columns.Add(new OLVColumn()
+                {
+                    Name = "Name",
+                    AspectGetter = x => ((IAction)x).Name,
+                    WordWrap = true,
+                    FillsFreeSpace = true,
+                });
+
+            Action.IconCache.EntryUpdated +=new EventHandler<LruCacheBackground<Action,Icon>.EntryUpdatedEventArgs>(IconCache_EntryUpdated);
+
+            this.Controls.Add(itemView);
+
+            InitializeComponent();
+
+            itemView.ItemActivate += new EventHandler(itemView_ItemsActivated);
+            itemView.GotFocus += new EventHandler(itemView_GotFocus);
+
+            itemView.ContextMenu = new ContextMenu(new MenuItem[]
+            {
+                new MenuItem("Activate", (s,e) =>
+                    {
+                        OnItemsActivated();
+                    }),
+                new MenuItem("Remove", (s,e) =>
+                    {
+                        Remove();
+                    }),
+                new MenuItem("Properties", (s,e) =>
+                    {
+                        Properties();
+                    }),
+            });
+
+            textBoxQuery.KeyDown += new KeyEventHandler(textBoxQuery_KeyDown);
+
+            ActionSource = actionSource;
+
+            textBoxQuery.GetTextChangedObservable()
+                .Throttle(TimeSpan.FromMilliseconds(200))
+                .Merge(ManualUpdate)
+                .Select(query => ActionSource.GetActions(query))
+                .ObserveOn(this)
+                .Subscribe(result =>
+                    {
+                        actions = new List<IAction>();
+                        result
+                            .ObserveOn(this)
+                            .Subscribe(_ =>
+                            {
+                                actions.Add(_);
+                                var si = Math.Max(itemView.SelectedIndex, 0);
+                                itemView.SetObjects(actions);
+                                itemView.SelectedIndex = si;
+                            });
+                    });
+        }
+
+        Subject<string> ManualUpdate = new Subject<string>();
+
+        void itemView_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            e.Item = new ListViewItem(actions[e.ItemIndex].Name);
+        }
+
+        IList<IAction> actions;
+        IList<IAction> Actions
+        {
+            set
+            {
+                actions = value;
+                itemView.SetObjects(actions);
+            }
+        }
+
+        void IconCache_EntryUpdated(object sender, LruCacheBackground<Action, Icon>.EntryUpdatedEventArgs arg)
+        {
+            this.BeginInvoke((MethodInvoker)delegate { itemView.BuildList(); });
+        }
+
+        void itemView_GotFocus(object sender, EventArgs e)
+        {
+            textBoxQuery.Focus();
+        }
+
+        public string Query
+        {
+            get
+            {
+                return textBoxQuery.Text;
+            }
+
+            set
+            {
+                textBoxQuery.Text = value;
+            }
+        }
+
+        public void Start()
+        {
+            textBoxQuery.SelectAll();
+        }
+
+        public event EventHandler ItemsActivated;
+
+        void itemView_ItemsActivated(object sender, EventArgs e)
+        {
+            OnItemsActivated();
+        }
+
+        void OnItemsActivated()
+        {
+            if (ItemsActivated != null)
+            {
+                ItemsActivated(this, EventArgs.Empty);
+            }
+        }
+
+        public IEnumerable<IAction> SelectedActions
+        {
+            get
+            {
+                return itemView.SelectedObjects.Cast<IAction>();
+            }
+        }   
+
+        public void Remove()
+        {
+            foreach (var i in itemView.SelectedObjects.OfType<ActionWrapper>())
+            {
+                i.Data.Remove(i.Action);
+            }
+            UpdateResult();
+        }
+
+        void textBoxQuery_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Down:
+                    if (itemView.SelectedIndex < itemView.GetItemCount())
+                    {
+                        SelectItem(itemView.SelectedIndex + 1);
+                    }
+                    e.Handled = true;
+                    break;
+                case Keys.Up:
+                    if (itemView.SelectedIndex > 0)
+                    {
+                        SelectItem(itemView.SelectedIndex - 1);
+                    }
+                    e.Handled = true;
+                    break;
+                case Keys.Enter:
+                    if (e.Alt)
+                    {
+                        Properties();
+                    }
+                    else
+                    {
+                        OnItemsActivated();
+                    }
+                    e.Handled = true;
+                    break;
+            }
+        }
+
+        public void UpdateResult()
+        {
+            ManualUpdate.OnNext(textBoxQuery.Text);
+        }
+
+        public void Properties()
+        {
+            var actionWrapper = SelectedActions.OfType<ActionWrapper>().FirstOrDefault();
+            if (actionWrapper != null)
+            {
+                ActionProperties dlg = new ActionProperties();
+                dlg.EditedObject = actionWrapper.Action;
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    actionWrapper.Data.Update(actionWrapper.Action);
+                }
+                UpdateResult();
+            }
+        }
+    }
+}

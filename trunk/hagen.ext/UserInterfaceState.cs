584418f70436102a3455d928a35ce256aebd75ca
andreas.grimme@googlemail.com <andreas.grimme@googlemail.com@6ee6a016-79e0-11de-888d-2103c0e6b660>
2014-12-14 06:32:40 +0000

    Automatic build of hagen

---
+++ trunk/hagen.ext/UserInterfaceState.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Automation;
+using Sidi.IO;
+
+namespace hagen
+{
+    public class UserInterfaceState
+    {
+        public static UserInterfaceState Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UserInterfaceState();
+                }
+                return instance;
+            }
+        }
+
+        static UserInterfaceState instance;
+
+        public void SaveFocus()
+        {
+            focusedElement = (IntPtr) NativeMethods.GetForegroundWindow();
+            try
+            {
+                selectedPathList = PathList.GetFilesSelectedInExplorer();
+            }
+            catch
+            {
+                selectedPathList = new PathList();
+            }
+        }
+
+        IntPtr focusedElement = IntPtr.Zero;
+
+        public AutomationElement SavedFocusedElement
+        {
+            get
+            {
+                if (focusedElement == IntPtr.Zero)
+                {
+                    return null;
+                }
+                return AutomationElement.FromHandle(focusedElement);
+            }
+        }
+        public PathList SelectedPathList
+        {
+            get
+            {
+                return selectedPathList;
+            }
+        }
+        PathList selectedPathList = new PathList();
+    }
+}
---
+++ trunk/hagen/DockExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WeifenLuo.WinFormsUI.Docking;
+using System.Windows.Forms;
+
+namespace hagen
+{
+    public static class DockExtension
+    {
+        public static DockContent AsDockContent(this Control c)
+        {
+            DockContent dockContent = new DockContent()
+            {
+                Text = c.Text,
+            };
+
+            c.Dock = DockStyle.Fill;
+            dockContent.Controls.Add(c);
+            return dockContent;
+        }
+    }
+}

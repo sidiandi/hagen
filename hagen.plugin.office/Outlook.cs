63a223f9453c63b65b9fd2c49ddc5e4ded154390
Andreas Grimme <andreas.grimme@gmx.net>
2016-04-15 05:43:07 +0200

    office plugin

---
+++ hagen.plugin.office/Outlook.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2016, Andreas Grimme
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sidi.Extensions;
+using Sidi.CommandLine;
+
+namespace Sidi
+{
+    [Usage("Outlook commands")]
+    class Outlook
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Outlook()
+        {
+        }
+
+        [Usage("Add a TO DO item")]
+        public void Todo(string subject)
+        {
+        }
+    }
+}
---
+++ hagen.plugin.office/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hagen.plugin.office
+{
+    class PluginFactory : IPluginFactory
+    {
+        public IEnumerable<IPlugin> CreatePlugins(IContext context)
+        {
+            var p = new Plugin();
+            p.Init(context);
+            return new[] { p };
+        }
+    }
+
+    class Plugin : ActionSourceCollector
+    {
+    }
+}

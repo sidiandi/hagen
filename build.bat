f48eb3a328f28e4efb7940a431daf8b9c40d28a8
Andreas Grimme <andreas.grimme@gmx.net>
2017-10-01 09:48:15 +0200

    GitVersion configuration

--- build.bat
+++ build.bat
@@ -5,7 +5,8 @@ if "%Target%" == "" (
 	set Target=Release
 )
 
-set msbuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe"
+rem set msbuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe"
+set msbuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 set SourceDir=%~dp0.
 call :file_name_from_path DirName %SourceDir%
 %msbuild% "%SourceDir%\build\Bootstrap.proj" /p:BuildTarget=%Target%

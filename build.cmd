826718c13861f9ce82ed70633da34193c0847a12
Andreas Grimme <andreas.grimme@gmx.net>
2017-10-03 07:56:49 +0200

    build.cmd returns errorlevel

--- build.cmd
+++ build.cmd
@@ -8,7 +8,7 @@ if "%Target%" == "" (
 set msbuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 set SourceDir=%~dp0.
 call :file_name_from_path DirName %SourceDir%
-%msbuild% "%SourceDir%\build\Bootstrap.proj" /p:BuildTarget=%Target%
+%msbuild% "%SourceDir%\build\Bootstrap.proj" /p:BuildTarget=%Target% || exit /b 1
 goto :eof
 
 :file_name_from_path <resultVar> <pathVar>

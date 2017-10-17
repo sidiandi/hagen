59ebde8155dc7534fc32f1ad7e28f6f4c8cac01f
Andreas Grimme <andreas.grimme@gmx.net>
2017-10-17 05:28:38 +0200

    new wix installer +semver: minor

--- setup/update-Files.cmd
+++
@@ -1,7 +0,0 @@
-set PRODUCTNAME=hagen
-set STAGE=%TEMP%\%PRODUCTNAME%-stage
-set WIXBIN=%ProgramFiles(x86)%\WiX Toolset v3.8\bin
-
-del /s /q %STAGE%
-robocopy /mir /s "C:\Users\Andreas\build\hagen\Release-AnyCPU" %STAGE% /xf *.xml /xf *.pdb /xd test
-"%WIXBIN%\heat" dir %STAGE% -cg ProductComponents -ag -sreg -sfrag -o %~dp0Files.wxs -var var.BuildDirectory -dr INSTALLFOLDER

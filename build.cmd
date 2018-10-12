40ea0c36fbd4ddc73b0eb98c37bd69319fbd7917
Andreas Grimme <andreas.grimme@siemens.com>
2018-10-12 05:24:07 +0200

    build script complete

--- build.cmd
+++ build.cmd
@@ -1,1 +1,16 @@
-@powershell %~dp0build.ps1 --target=%1
+@echo off
+
+set help=0
+if "%1" == "--help" set help=1
+if "%1" == "-h" set help=1
+if "%1" == "-*" set help=1
+
+if 1 EQU %help% (
+	set args=--showdescription
+) else (
+	set target=%1
+	if "%target%" == "" set target=Default
+	set args=--target=%target%
+)
+
+@powershell %~dp0build.ps1 %args%
